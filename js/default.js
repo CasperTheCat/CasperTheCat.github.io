b2b632faff5c61f00ca4620fdcb8befcddff294e
Chris Anderson <zsmyna@gmail.com>
2016-05-11 19:45:38 +1200

    New style on Index and Artwork.
    Need to settle on a content design

--- js/default.js
+++ js/default.js
@@ -1,19 +1,19 @@
-﻿$(window).load(function () {
-    $('.landTitle').find('img').each(function () {
-        var imgClass = ($(document).width() / 16 > $(document).height() / 9) ? 'wide' : 'tall';
-        //alert($(document).width());
-        //alert($(document).height());
-        $(this).addClass(imgClass);
-    })
-})
+﻿//$(window).load(function () {
+//    $('.landTitle').find('img').each(function () {
+//        var imgClass = ($(document).width() / 16 > $(document).height() / 9) ? 'wide' : 'tall';
+//        //alert($(document).width());
+//        //alert($(document).height());
+//        $(this).addClass(imgClass);
+//    })
+//})
+//
 
-
-$(window).resize(function () {
-    $('.landTitle').find('img').each(function () {
-        var imgClass = ($(document).width() / 16 > $(document).height() / 9 ) ? 'wide' : 'tall';
-        //alert($(document).width());
-        //alert($(document).height());
-        $(this).addClass(imgClass);
-    })
-})
+//$(window).resize(function () {
+//    $('.landTitle').find('img').each(function () {
+//        var imgClass = ($(document).width() / 16 > $(document).height() / 9 ) ? 'wide' : 'tall';
+//        //alert($(document).width());
+//        //alert($(document).height());
+//        $(this).addClass(imgClass);
+//    })
+//})
 
