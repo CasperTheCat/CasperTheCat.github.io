503e8afbb9f050ca418bc3a2dd34b493b0d0c348
Chris Anderson <zsmyna@gmail.com>
2016-05-11 18:18:44 +1200

    Just fixing EVERYTHING
    brb more commits to come
    
    Signed-off-by: Chris Anderson <zsmyna@gmail.com>

--- js/default.js
+++ js/default.js
@@ -1,53 +1,19 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkId=232509
+﻿$(window).load(function () {
+    $('.landTitle').find('img').each(function () {
+        var imgClass = ($(document).width() / 16 > $(document).height() / 9) ? 'wide' : 'tall';
+        //alert($(document).width());
+        //alert($(document).height());
+        $(this).addClass(imgClass);
+    })
+})
 
 
-//(function () {
-//    "use strict";
+$(window).resize(function () {
+    $('.landTitle').find('img').each(function () {
+        var imgClass = ($(document).width() / 16 > $(document).height() / 9 ) ? 'wide' : 'tall';
+        //alert($(document).width());
+        //alert($(document).height());
+        $(this).addClass(imgClass);
+    })
+})
 
-//    var app = WinJS.Application;
-//    var activation = Windows.ApplicationModel.Activation;
-
-//    app.onactivated = function (args) {
-//        if (args.detail.kind === activation.ActivationKind.launch) {
-//            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-//                // TODO: This application has been newly launched. Initialize
-//                // your application here.
-//            } else {
-//                // TODO: This application has been reactivated from suspension.
-//                // Restore application state here.
-//            }
-//            args.setPromise(WinJS.UI.processAll());
-//        }
-//    };
-
-//    app.oncheckpoint = function (args) {
-//        // TODO: This application is about to be suspended. Save any state
-//        // that needs to persist across suspensions here. You might use the
-//        // WinJS.Application.sessionState object, which is automatically
-//        // saved and restored across suspension. If you need to complete an
-//        // asynchronous operation before your application is suspended, call
-//        // args.setPromise().
-//    };
-
-//    app.start();
-//})();
-
-//window.onload = testFunc;
-
-//function testOver(element) {
-//    var target = $(element.target);
-//    alert("HE");
-//}
-
-//function testFunc() {
-//    alert("hi");
-
-//    var navItems = document.getElementById("navMenu").getElementsByTagName("li");
-//    alert(navItems.length);
-
-//    for (var i = 0; i < navItems.length; i++) // Valid items only please
-//    {
-//        navItems[i].addEventListener("mouseover",testOver);
-//    }
-//}
